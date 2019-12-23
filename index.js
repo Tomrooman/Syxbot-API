798abf805d4ff6d9ccd4ff0b1039288d7cb826ef
Tom Rooman <tomrooman@hotmail.com>
2019-12-23 13:25:18 +0100

    add 404 message

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const app = express();
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 
-app.post('/docs/contact', function (req, res) {
+app.post('/docs/contact', (req, res) => {
     console.log('req body : ', req.body)
     res.send('Données reçues');
 });
+
+app.get('/*', (req, res) => {
+    res.send('404 not found')
+})
 
 https.createServer({
     key: fs.readFileSync('./server.key'),
